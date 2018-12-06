047e3b0272fbb96d8795fd92264a15979a916610
910JQK <v910JQK@gmail.com>
2018-12-06 19:14:52 +0800

    Add Frontend

---
+++ chat.js
@@ -0,0 +1,136 @@
+const URL = 'ws://127.0.0.1:8102'
+
+
+var socket
+
+
+function is_scrolled_to_bottom(view) {
+    return view.scrollTop == view.scrollTopMax
+}
+
+
+function scroll_to_bottom(view) {
+    return view.scrollTop = view.scrollTopMax
+}
+
+
+function 渲染消息 (消息视图) {
+    let 滚动到最后 = is_scrolled_to_bottom(消息列表视图)    
+    消息列表视图.appendChild(消息视图)
+    if ( 滚动到最后 ) {
+        scroll_to_bottom(消息列表视图)
+    }    
+}
+
+
+var 如何处理消息 = [
+    {
+        类型: one_of('说话'),
+        执行动作: function (消息) {
+            let 谁 = 消息.内容.谁
+            let 说了什么 = 消息.内容.说了什么
+            渲染消息(create({
+                tag: 'message',
+                dataset: { 消息类型: 消息.类型 },
+                children: [
+                    { tag: 'recv-time', textContent: `(${消息.收到时间})` },
+                    { tag: 'say-name', textContent: 谁 },
+                    { tag: 'say-content', textContent: 说了什么 }
+                ]
+            }))
+        }
+    },
+    {
+        类型: one_of('通知', '反馈'),
+        执行动作: function (消息) {
+            渲染消息(create({
+                tag: 'message',
+                dataset: { 消息类型: 消息.类型 },
+                children: [
+                    { tag: 'recv-time', textContent: `(${消息.收到时间})` },
+                    { tag: 'info', textContent: 消息.内容 }
+                ]
+            }))
+        }
+    },
+    {
+        类型: one_of('用户列表'),
+        执行动作: function (消息) {
+            let 用户列表 = 消息.内容
+            用户列表.sort()
+            用户数量视图.textContent = `当前 ${用户列表.length} 人在线`
+            clear(用户列表视图)
+            map(用户列表, 名字 => 用户列表视图.appendChild(create({
+                tag: 'user-item',
+                textContent: 名字
+            })))
+        }
+    }
+]
+
+
+function 处理消息 (消息) {
+    消息.收到时间 = (new Date()).toTimeString().replace(/ .*/, '')
+    for ( let 模式 of 如何处理消息 ) {
+        if (模式.类型.has(消息.类型)) {
+            模式.执行动作(消息)
+        }
+    }
+}
+
+
+function 发送消息 (消息) {
+    socket.send(JSON.stringify(消息))
+}
+
+
+function 说话 (说了什么) {
+    发送消息({ 命令: '说话', 说了什么: 说了什么 })
+}
+
+
+function 改名 (新名字) {
+    发送消息({ 命令: '改名', 新名字: 新名字 })
+}
+
+
+var handlers = {
+    open: function (ev) {
+        console.log('Connected')
+    },
+    close: function (ev) {
+        console.log('Disconnected')
+    },
+    message: function (ev) {
+        处理消息(JSON.parse(ev.data))
+    }
+}
+
+
+function init () {
+    socket = new WebSocket(URL)
+    map(handlers, (event, handler) => socket.addEventListener(event, handler))
+    输入框.addEventListener('keyup', function (ev) {
+        if (ev.key == 'Enter') {
+            let 说了什么 = 输入框.value.trimRight()
+            if (说了什么) {
+                说话(说了什么)
+                输入框.value = ''
+            }
+        }
+    })
+    输入框.addEventListener('keypress', function (ev) {
+        if (ev.key == 'Enter') {
+            ev.preventDefault()
+        }
+    })
+    改名按钮.addEventListener('click', function (ev) {
+        let 新名字 = prompt('请输入新的名字')
+        if (新名字 != null && 新名字 != '') {
+            改名(新名字)
+        }
+    })
+}
+
+
+window.addEventListener('DOMContentLoaded', init)
---
+++ toolkit.js
@@ -0,0 +1,137 @@
+function one_of(...str_list) {
+    return new Set(str_list)
+}
+
+
+function map (to_be_mapped, f) {
+    let result = []
+    if( to_be_mapped[Symbol.iterator] ) {
+	let iterable = to_be_mapped
+	let index = 0
+	for ( let I of iterable ) {
+	    result.push(f(I, index))
+	    index += 1
+	}
+    } else {
+	let hash = to_be_mapped
+	for ( let key of Object.keys(hash) ) {
+	    result.push(f(key, hash[key]))
+	}
+    }
+    return result
+}
+
+
+function filter (object, f) {
+  if ( object[Symbol.iterator] ) {
+    let iterable = object
+    let result = []
+    for ( let element of iterable ) {
+      if( f(element) ) {
+	result.push(element)
+      }
+    }
+    return result
+  } else {
+    let hash = object
+    let result = {}
+    for ( let key of Object.keys(hash) ) {
+      if( f(key, hash[key]) ) {
+	result[key] = hash[key]
+      }
+    }
+    return result
+  }
+}
+
+
+function filter_key (hash, f) {
+    var result = {}
+    for ( let key of Object.keys(hash) ) {
+        if ( f(key, hash[key]) ) {
+            result[key] = hash[key]
+        }
+    }
+    return result
+}
+
+
+function clear(element) {
+    while ( element.firstChild ) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+
+function create (data) {
+    if ( data instanceof HTMLElement ) { return data }
+    let element = document.createElement(data.tag)
+    element._fields = {}
+    map(data.classList || [], name => element.classList.add(name))
+    map(filter(['style', 'dataset'], p => data.has(p)), function(property) {
+        map(data[property], function (key, value) {
+            if ( value instanceof field ) {
+                let field = value
+                element._fields[field.name] = {
+                    read: () => element[property][key],
+                    update: value => element[property][key] = value
+                }
+                if ( field.has('initial') ) {
+                    element[property][key] = initial
+                }
+            } else {
+                element[property][key] = data[property][key]
+            }
+        })
+    })
+    map(data.handlers || {}, (ev, f) => element.addEventListener(ev, f))
+    let reserved = one_of(
+        'classList', 'style', 'dataset', 'handlers', 'children'
+    )
+    map(filter_key(data, k => !reserved.has(k)), function (property, value) {
+        if ( value instanceof field ) {
+            let field = value
+            element._fields[field.name] = {
+                read: () => element[property],
+                update: value => element[property] = value
+            }
+            if ( field.has('initial') ) {
+                element[property] = field.initial
+            }
+        } else {
+            element[property] = value
+        }
+    })
+    map(data.children || [], child => element.appendChild(create(child)) )
+    return element
+}
+
+
+function field (name, initial) {
+    var object = Object.create(field.prototype)
+    object.name = name
+    object.initial = initial
+    return field_object
+}
+
+
+function read (element, field_name) {
+    if ( element._fields.has(field_name) ) {
+        return element._fields[field_name].read.call(element)
+    }
+    for ( let child of element.children ) {
+        let result = read(child, field_name)
+        if ( typeof result != 'undefined' ) {
+            return result
+        }
+    }
+}
+
+
+function update (element, data) {
+    map(element._fields, (key, field) => field.update.call(element, data[key]))
+    map(element.children, child => update(child, data))
+}
+
+
+Object.prototype.has = function (key) { return this.hasOwnProperty(key) }
